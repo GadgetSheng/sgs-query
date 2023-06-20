e9a446ef5a899a9591e97cc7d712329e64532f1d
jqsheng <jqsheng@trip.com>
2023-06-20 16:26:31 +0800

    feat: add vConsole

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import VConsole from 'vconsole';
 import './index.css'
 import 'virtual:uno.css';
+
+const vConsole=new VConsole();
+window.addEventListener('destroy',()=>{
+  vConsole.destroy();
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
